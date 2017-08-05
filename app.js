50875d2c505ec47c17053f719213f565b10bb93c
Khalil Hughes <hughes.khalil@gmail.com>
2017-08-04 21:22:43 -0400

    added D from CRUD

--- app.js
+++ app.js
@@ -100,6 +100,17 @@ app.put("/blogs/:id", function(req, res) {
   });
 });
 
+//DELETE ROUTE
+app.delete("/blogs/:id", function(req, res) {
+  Blog.findByIdAndRemove(req.params.id, function(err) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs");
+    }
+  });
+});
+
 app.listen(server, function() {
   console.log("Restful blog app serving on port " + server);
 });
